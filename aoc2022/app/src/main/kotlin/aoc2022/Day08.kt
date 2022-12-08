18d05d1c4b900507110c2b39507f85c4d8583863
resurtm <resurtm@gmail.com>
2022-12-08 12:23:42 +0100

    Add AoC 2022, day 08 solution, part 2 done

--- aoc2022/app/src/main/kotlin/aoc2022/Day08.kt
+++ aoc2022/app/src/main/kotlin/aoc2022/Day08.kt
@@ -25,10 +25,14 @@ package aoc2022
 import java.io.File
 
 fun solveDay08() {
-    solveDay08Internal(DataFileType.GOOGLE)
+    for (dft in DataFileType.values()) {
+        solveDay08Internal(dft)
+    }
 }
 
 private fun solveDay08Internal(dft: DataFileType) {
+    grid = mutableListOf<MutableList<Tree>>()
+
     File(getDataFilePath(8, dft)).forEachLine {
         readLine(it)
     }
@@ -43,12 +47,24 @@ private fun solveDay08Internal(dft: DataFileType) {
                 max = grid[j][i].char
                 grid[j][i].visible = true
             }
+            for (i2 in i - 1 downTo 0) {
+                grid[j][i].left++
+                if (grid[j][i].char <= grid[j][i2].char) {
+                    break
+                }
+            }
         }
         max = '/'
         for (i in sizeI - 1 downTo 0) {
             if (max < grid[j][i].char) {
                 max = grid[j][i].char
                 grid[j][i].visible = true
+            }
+            for (i2 in i + 1 until sizeI) {
+                grid[j][i].right++
+                if (grid[j][i].char <= grid[j][i2].char) {
+                    break
+                }
             }
         }
     }
@@ -59,6 +75,12 @@ private fun solveDay08Internal(dft: DataFileType) {
                 max = grid[j][i].char
                 grid[j][i].visible = true
             }
+            for (j2 in j - 1 downTo 0) {
+                grid[j][i].top++
+                if (grid[j][i].char <= grid[j2][i].char) {
+                    break
+                }
+            }
         }
         max = '/'
         for (j in sizeJ - 1 downTo 0) {
@@ -66,31 +88,52 @@ private fun solveDay08Internal(dft: DataFileType) {
                 max = grid[j][i].char
                 grid[j][i].visible = true
             }
+            for (j2 in j + 1 until sizeJ) {
+                grid[j][i].bottom++
+                if (grid[j][i].char <= grid[j2][i].char) {
+                    break
+                }
+            }
         }
     }
 
     var count = 0
     for (j in 0 until sizeJ) {
         for (i in 0 until sizeI) {
-//            print(if (grid[j][i].visible) '1' else '0')
             if (grid[j][i].visible) {
                 count++
             }
         }
-//        println()
     }
-    println(count)
+
+    var max = 0
+    for (j in 0 until sizeJ) {
+        for (i in 0 until sizeI) {
+            val score = grid[j][i].left * grid[j][i].right * grid[j][i].top * grid[j][i].bottom
+            if (max < score) {
+                max = score
+            }
+        }
+    }
+
+    println("Type: ${dft.value}. Part 1: $count. Part 2: $max.")
 }
 
-private data class Tree(val char: Char, var visible: Boolean)
+private data class Tree(
+    val char: Char,
+    var visible: Boolean,
+    var left: Int = 0,
+    var right: Int = 0,
+    var top: Int = 0,
+    var bottom: Int = 0
+)
 
 private var sizeI = 0
 private var sizeJ = 0
-private val grid = mutableListOf<MutableList<Tree>>()
+private var grid = mutableListOf<MutableList<Tree>>()
 
 private fun readLine(line: String) {
     val row = mutableListOf<Tree>()
     line.forEach { row.add(Tree(it, false)) }
     grid.add(row)
 }
-
