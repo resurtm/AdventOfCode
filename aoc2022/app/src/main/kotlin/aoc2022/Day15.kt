a570238b969768d7bae27423fa55294b020a769f
resurtm <resurtm@gmail.com>
2022-12-15 16:40:16 +0100

    Add AoC 2022, day 15, part 1, solution

--- aoc2022/app/src/main/kotlin/aoc2022/Day15.kt
+++ aoc2022/app/src/main/kotlin/aoc2022/Day15.kt
@@ -23,16 +23,68 @@
 package aoc2022
 
 import java.io.File
+import kotlin.math.abs
 
 fun solveDay15() {
-    val day15Solution = Day15Solution(DataFileType.EXAMPLE)
-    day15Solution.readFile()
+    val day15Solution1 = Day15Solution(DataFileType.EXAMPLE)
+    day15Solution1.readFile()
+    day15Solution1.solvePart1(10)
+
+    val day15Solution2 = Day15Solution(DataFileType.GITHUB)
+    day15Solution2.readFile()
+    day15Solution2.solvePart1(2000000)
+
+    val day15Solution3 = Day15Solution(DataFileType.GOOGLE)
+    day15Solution3.readFile()
+    day15Solution3.solvePart1(2000000)
 }
 
 private class Day15Solution(val dft: DataFileType) {
-    fun readFile() {
-        File(getDataFilePath(15, dft)).forEachLine { parseLine(it) }
+    fun solvePart1(neededY: Int) {
+        sensors.forEach { sensor ->
+            calcInterval(sensor, neededY)?.let { intervals.add(it) }
+        }
+        val merged = mergedIntervals()
+        val result = merged.first().second - merged.first().first
+        println("Day 15. Data file type: ${dft}/${dft.value}. Part 1: $result.")
     }
+
+    private fun mergedIntervals(): MutableList<Pair<Int, Int>> {
+        val intvals = intervals.sortedWith(IntervalComparator)
+        val merged = mutableListOf(intvals[0].copy())
+        for (interval in intvals) {
+            val last = merged.last()
+            if (
+                last.first <= interval.first &&
+                last.second >= interval.first &&
+                last.second <= interval.second
+            ) {
+                merged[merged.size - 1] = Pair(last.first, interval.second)
+            } else if (
+                last.first <= interval.first &&
+                last.second >= interval.second
+            ) {
+                // do nothing
+            } else if (
+                last.first >= interval.first &&
+                last.second <= interval.second
+            ) {
+                merged[merged.size - 1] = interval.copy()
+            } else {
+                merged.add(interval.copy())
+            }
+        }
+        return merged
+    }
+
+    private fun calcInterval(sen: Sensor, neededY: Int): Pair<Int, Int>? {
+        val coverage = abs(sen.pos.x - sen.beacon.x) + abs(sen.pos.y - sen.beacon.y)
+        val yDist = abs(sen.pos.y - neededY)
+        if (yDist > coverage) return null
+        return Pair(sen.pos.x - (coverage - yDist), sen.pos.x + (coverage - yDist))
+    }
+
+    fun readFile() = File(getDataFilePath(15, dft)).forEachLine { parseLine(it) }
 
     private fun parseLine(inputLine: String) {
         val matchResult = lineRegex.matchEntire(inputLine) ?: return
@@ -43,15 +95,29 @@ private class Day15Solution(val dft: DataFileType) {
         val closestBeaconX = matchResult.groups[3]?.value?.toInt() ?: return
         val closestBeaconY = matchResult.groups[4]?.value?.toInt() ?: return
 
-        val sensor = Sensor(sensor = Point(sensorX, sensorY), closestBeacon = Point(closestBeaconX, closestBeaconY))
+        val sensor = Sensor(pos = Point(sensorX, sensorY), beacon = Point(closestBeaconX, closestBeaconY))
         sensors = sensors.plus(sensor)
     }
 
     private var sensors = emptyList<Sensor>()
+    private val intervals = mutableListOf<Pair<Int, Int>>()
     private val lineRegex = Regex(
         "Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)"
     )
 
-    private data class Sensor(val sensor: Point, val closestBeacon: Point)
+    private data class Sensor(val pos: Point, val beacon: Point)
     private data class Point(val x: Int = 0, val y: Int = 0)
+
+    private class IntervalComparator {
+        companion object : Comparator<Pair<Int, Int>> {
+            override fun compare(a: Pair<Int, Int>, b: Pair<Int, Int>): Int {
+                val second = if (a.second == b.second) 0
+                else if (a.second < b.second) -1
+                else 1
+                return if (a.first == b.first) second
+                else if (a.first < b.first) -1
+                else 1
+            }
+        }
+    }
 }
